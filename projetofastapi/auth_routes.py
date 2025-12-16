c885f06bf275f74f36912800f91369c62cf448d6
DaviLuckas <davilucasgx@gmail.com>
2025-12-15 22:54:29 -0300

    feat: create query to filter database information

--- projetofastapi/auth_routes.py
+++ projetofastapi/auth_routes.py
@@ -13,6 +13,14 @@ async def home():
     return {"mensagem" : "Você acessou a rota padrão de autenticação", "autenticado": False}
 
 @auth_router.post("/cria_conta")
-async def criar_conta(email: str, senha: str):
+async def criar_conta(email: str, senha: str, nome: str):
     Session = sessionmaker(bind=db)
     session = Session()
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if usuario:
+        return{"mensagem": "já existe um usuario com esse email!"}
+    else:
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        session.commit()
+        return{"mensagem": "usuario cadastrado com sucesso!"}
