7914c83e66e84247965af19c8950d5a6719ea7f4
DaviLuckas <davilucasgx@gmail.com>
2025-12-15 23:15:59 -0300

    fix: ensure session is closed using try/finally in dependency

--- projetofastapi/dependencies.py
+++ projetofastapi/dependencies.py
@@ -2,6 +2,9 @@ from models import db
 from sqlalchemy.orm import sessionmaker
 
 def pegar_sessao():
-    Session = sessionmaker(bind=db)
-    session = Session()
-    return session
+    try:
+        Session = sessionmaker(bind=db)
+        session = Session()
+        yield session
+    finally:
+        session.close()
